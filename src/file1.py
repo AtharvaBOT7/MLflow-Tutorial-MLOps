c328bc1e8d6079dc6a78f0996471dfc55d340542
atharvaBOT7 <atharvachundurwar841@gmail.com>
2025-07-30 18:33:05 -0700

    basic requirements added, imports in rough file1 added

---
+++ src/file1.py
@@ -0,0 +1,8 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
